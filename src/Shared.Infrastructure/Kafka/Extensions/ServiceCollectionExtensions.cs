0754a4f3c56958f9dfec9e861682d41a17bd130d
Daniil Dukhovnyi <dduhovny26@gmail.com>
2023-01-14 01:53:47 +0300

    Добавление поддержки кафки в общий проект инфраструктуры

---
+++ src/Shared.Infrastructure/Kafka/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using KafkaFlow;
+using KafkaFlow.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Shared.Infrastructure.Kafka.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddKafkaEventLogging(this IServiceCollection services,
+        Action<IClusterConfigurationBuilder> builderAction)
+    {
+        services.AddKafka(kafka =>
+        {
+            kafka.UseMicrosoftLog();
+            kafka.AddCluster(builderAction);
+        });
+        
+        return services;
+    }
+}
---
+++ src/Shared.Infrastructure/Kafka/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,15 @@
+﻿using KafkaFlow;
+using Microsoft.AspNetCore.Builder;
+
+namespace Shared.Infrastructure.Kafka.Extensions;
+
+public static class WebApplicationExtensions
+{
+    public static WebApplication UseKafkaBus(this WebApplication app)
+    {
+        var bus = app.Services.CreateKafkaBus();
+        app.Lifetime.ApplicationStarted.Register(() => bus.StartAsync(app.Lifetime.ApplicationStopped));
+        
+        return app;
+    }
+}
