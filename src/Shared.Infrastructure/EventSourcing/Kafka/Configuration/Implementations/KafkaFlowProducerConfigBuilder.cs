4c159178071eecd7bb0f391d17a837a86a6f8447
Daniil Dukhovnyi <dduhovny26@gmail.com>
2023-03-10 00:07:55 +0300

    Реализация билдера конфигураций для консумера и продюсера

---
+++ src/Shared.Infrastructure/EventSourcing/Kafka/Configuration/Implementations/KafkaFlowProducerConfigBuilder.cs
@@ -0,0 +1,57 @@
+﻿using KafkaFlow;
+using KafkaFlow.Configuration;
+using KafkaFlow.Serializer.SchemaRegistry;
+using Shared.Infrastructure.EventSourcing.Kafka.Configuration.Interfaces;
+using Shared.Infrastructure.EventSourcing.Kafka.Middlewares;
+using Shared.Infrastructure.EventSourcing.Kafka.Resolvers;
+
+namespace Shared.Infrastructure.EventSourcing.Kafka.Configuration.Implementations;
+
+public sealed class KafkaFlowProducerConfigBuilder : IKafkaProducerConfigBuilder,
+    IConfigBuilder<Action<IClusterConfigurationBuilder>>
+{
+    private string? _name;
+    private string? _defaultTopic;
+    
+    /// <inheritdoc />
+    public IKafkaProducerConfigBuilder SetName(string name)
+    {
+        _name = name;
+        return this;
+    }
+
+    /// <inheritdoc />
+    public IKafkaProducerConfigBuilder SetDefaultTopic(string topic)
+    {
+        _defaultTopic = topic;
+        return this;
+    }
+
+    /// <inheritdoc />
+    public Action<IClusterConfigurationBuilder> Build()
+    {
+        if (_name is null)
+        {
+            throw new InvalidOperationException("Producer name was not specified");
+        }
+
+        Action<IClusterConfigurationBuilder> configAction = cluster =>
+        {
+            cluster.AddProducer(_name, producer =>
+            {
+                if (!string.IsNullOrWhiteSpace(_defaultTopic))
+                {
+                    producer.DefaultTopic(_defaultTopic);
+                }
+
+                producer.AddMiddlewares(mw =>
+                {
+                    mw.AddAtBeginning<ExceptionHandlerKafkaMiddleware>();
+                    mw.AddSerializer<ConfluentProtobufSerializer, CustomTypeKafkaResolver>();
+                });
+            });
+        };
+
+        return configAction;
+    }
+}
---
+++ src/Shared.Infrastructure/EventSourcing/Kafka/Resolvers/CustomTypeKafkaResolver.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using KafkaFlow;
+
+namespace Shared.Infrastructure.EventSourcing.Kafka.Resolvers;
+
+public sealed class CustomTypeKafkaResolver : IMessageTypeResolver
+{
+    private const string MessageType = "MessageType";
+    
+    /// <inheritdoc />
+    public Type OnConsume(IMessageContext context)
+    {
+        var typeName = context.Headers.GetString(MessageType) + ", " + Assembly.GetExecutingAssembly().GetName().Name;
+
+        return Type.GetType(typeName)!;
+    }
+
+    /// <inheritdoc />
+    public void OnProduce(IMessageContext context)
+    {
+        context.Headers.SetString(MessageType, context.Message.Value.GetType().FullName);
+    }
+}
