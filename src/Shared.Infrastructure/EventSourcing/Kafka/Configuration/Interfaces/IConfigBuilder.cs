f3f471b99783f2f15dffa484b0700acb36c34bd0
Daniil Dukhovnyi <dduhovny26@gmail.com>
2023-03-10 00:07:27 +0300

    Интерфейсы для билдера конфигурации кафки

---
+++ src/Shared.Infrastructure/EventSourcing/Kafka/Configuration/Interfaces/IConfigBuilder.cs
@@ -0,0 +1,6 @@
+﻿namespace Shared.Infrastructure.EventSourcing.Kafka.Configuration.Interfaces;
+
+public interface IConfigBuilder<out TResult>
+{
+    TResult Build();
+}
---
+++ src/Shared.Infrastructure/EventSourcing/Kafka/Configuration/Interfaces/IKafkaConfigBuilder.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Shared.Infrastructure.EventSourcing.Kafka.Configuration.Interfaces;
+
+public interface IKafkaConfigBuilder
+{
+    IServiceCollection Services { get; set; }
+
+    /// <summary>
+    /// Задание адресов брокеров
+    /// </summary>
+    /// <param name="brokerAddresses">Адреса для брокеров из кластера (адреса и порты)</param>
+    /// <returns></returns>
+    IKafkaConfigBuilder UseBrokers(params string[] brokerAddresses);
+
+    /// <summary>
+    /// Добавление Schema Registry
+    /// </summary>
+    /// <param name="schemaRegistryName">Адрес Schema Registry с указанием протокола (http и https)</param>
+    /// <returns></returns>
+    IKafkaConfigBuilder UseSchemaRegistry(string schemaRegistryName);
+    
+    /// <summary>
+    /// Регистрация консумера с нужным конфигом
+    /// <param name="configAction"></param>
+    /// </summary>
+    /// <returns></returns>
+    IKafkaConfigBuilder RegisterConsumer(Action<IKafkaConsumerConfigBuilder> configAction);
+    
+    /// <summary>
+    /// Регистрация продюсера с нужным конфигом
+    /// </summary>
+    /// <param name="configAction"></param>
+    /// <returns></returns>
+    IKafkaConfigBuilder RegisterProducer(Action<IKafkaProducerConfigBuilder> configAction);
+}
