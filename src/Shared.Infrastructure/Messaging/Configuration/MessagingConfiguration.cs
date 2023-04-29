2f8b0f15e2807609e81ccec6776817ece7aedc83
Daniil Dukhovnyi <dduhovny26@gmail.com>
2023-04-29 22:31:14 +0300

    Новая система работы с кафкой и outbox

---
+++ src/Shared.Infrastructure/Messaging/Configuration/MessagingConfiguration.cs
@@ -0,0 +1,21 @@
+﻿namespace Shared.Infrastructure.Messaging.Configuration;
+
+public sealed class MessagingConfiguration
+{
+    public const string SectionName = "Messaging";
+
+    public string KafkaAddress { get; set; } = string.Empty;
+    
+    public int ConsumerThreadCount { get; set; } = 1;
+    public int ProducerThreadCount { get; set; } = 1;
+    
+    public string ConsumerGroupName { get; set; } = string.Empty;
+    
+    public int SucceedMessageExpiredAfterSeconds { get; set; } = (int)TimeSpan.FromDays(1).TotalSeconds;
+    public int FailedMessageExpiredAfterSeconds { get; set; } = (int)TimeSpan.FromDays(30).TotalSeconds;
+    
+    public int FailedRetryCount { get; set; } = 100;
+    public int FailedRetryIntervalSeconds { get; set; } = (int)TimeSpan.FromMinutes(2).TotalSeconds;
+
+    public int CleanupIntervalSeconds { get; set; } = (int)TimeSpan.FromMinutes(30).TotalSeconds;
+}
